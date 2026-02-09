daa320d493346928ef0c521ba089c49f441005f4
Asaiy420 <kcaryan05@gmail.com>
2026-02-09 12:01:49 +0545

    refactor server.ts for code consistency and formatting

--- user-service/src/server.ts
+++ user-service/src/server.ts
@@ -1,12 +1,16 @@
+import type { Request, Response } from 'express';
 import express from 'express';
-import "dotenv/config";
+import 'dotenv/config';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
 app.use(express.json());
 
+app.get('/', (req: Request, res: Response) => {
+  res.send('User service is up and running!');
+});
+
 app.listen(PORT, () => {
-    console.log(`User service is running on port ${PORT}`);
-})
+  console.log(`User service is running on port ${PORT}`);
+});
