128dd38bd79a806aa49f9ae2333bb65aee95d016
Asaiy420 <kcaryan05@gmail.com>
2026-02-09 12:49:16 +0545

    implement user creation functionality with MongoDB integration in server.ts

--- user-service/src/server.ts
+++ user-service/src/server.ts
@@ -1,12 +1,66 @@
 import type { Request, Response } from 'express';
 import express from 'express';
 import 'dotenv/config';
+import mongoose from 'mongoose';
+
+interface IUser extends mongoose.Document {
+  name: string;
+  email: string;
+}
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded());
+
+const conn = async () => {
+  try {
+    await mongoose.connect('mongodb://localhost:27017/user');
+    console.log('MongoDB Connected Successfully');
+  } catch (e: unknown) {
+    console.error('Error when connecting to the database', e);
+  }
+};
+
+// Creating User Schema Here; Will do everything in this file no different file structure
+
+const userSchema = new mongoose.Schema<IUser>({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+// Defining User Routes here
+
+app.post('/users', async (req: Request, res: Response) => {
+  try {
+    const { name, email } = req.body;
+
+    if (!name || !email) {
+      res.status(400).json({ message: 'Name and Email are required' });
+      return;
+    }
+    const user = new User({ name, email });
+    await user.save();
+
+    res.status(201).json({ message: 'User Created Successfully!', user });
+    return;
+  } catch (e: unknown) {
+    console.error('Error when creating a user', e);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+
 
 app.get('/', (req: Request, res: Response) => {
   res.send('User service is up and running!');
@@ -14,4 +68,5 @@ app.get('/', (req: Request, res: Response) => {
 
 app.listen(PORT, () => {
   console.log(`User service is running on port ${PORT}`);
+  conn();
 });
